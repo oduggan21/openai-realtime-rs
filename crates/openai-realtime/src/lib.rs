df1396475c5e74f7c6ee1dbd02672858d2e836a4
revlentless <215957173+revlentless@users.noreply.github.com>
2025-08-07 07:23:58 +0000

    refactor(openai-realtime): remove utils feature and deps to decouple

--- crates/openai-realtime/src/lib.rs
+++ crates/openai-realtime/src/lib.rs
@@ -1,7 +1,5 @@
 mod client;
 
 pub use openai_realtime_types as types;
-pub use client::{connect, Client, ServerRx};
 
-#[cfg(feature = "utils")]
-pub use feynman_native_utils as utils;
+pub use client::{connect, Client, ServerRx};
